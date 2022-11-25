385829a6c814a8cb7908499ed7119a0db16354da
dmhenry <davermont@gmail.com>
2022-11-24 19:05:57 -0500

    Add hello world

--- app/controllers/applications_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# app/controllers/application_controller.rb
-class ApplicationsController < ActionController::Base
-  def hello
-    render html: 'hello, world 2!'
-  end
-end
